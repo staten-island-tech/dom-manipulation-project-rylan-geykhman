29531d91421b72906a2c55510ad2d6b63e99552d
rylan-geykhman <144476581+rylan-geykhman@users.noreply.github.com>
2023-10-16 13:13:33 -0400

    hehehehaw

--- dom.js
+++ dom.js
@@ -1,13 +1,13 @@
 DOMSelectors = {
-    form: document.querySelector("form"),
-    firstName: document.querySelector("first-name"),
+    form: document.querySelector(".form"),
+    firstName: document.querySelector(".firstName"),
     ssn: document.querySelector("ssn"),
     address: document.querySelector("address"),
     credit_card: document.querySelector("credit-card"),
     object: document.querySelector("object"),
 };
 
-DOMSelectors.form.addEventListener("submit", function (event) {
-    event.preventDefault();
-    DOMSelectors.object.forEach((obj)=> obj.Content = DOMSelectors.form.value = "yes");
+DOMSelectors.firstName.addEventListener("click", function () {
+    DOMSelectors.firstName.innerHTML = Rylan
 });
+console.log(DOMSelectors.firstName)
