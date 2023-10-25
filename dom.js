70af33824fda8a9cc12e3f80ec3e866c12a08bac
rylan-geykhman <144476581+rylan-geykhman@users.noreply.github.com>
2023-10-25 13:12:20 -0400

    submit

--- dom.js
+++ dom.js
@@ -1,9 +1,14 @@
 DOMSelectors = {
     form: document.querySelector(".form"),
     firstName: document.querySelector(".firstName"),
+    h2: document.getElementById("h2"),
 };
 
 DOMSelectors.firstName.addEventListener("submit", function () {
-    DOMSelectors.firstName.innerHTML = 
+    DOMSelectors.firstName.insertAdjacentHTML("name")
 });
 console.log(DOMSelectors.firstName)
+
+//const ... = make...()
+//function ...()
+//remove button
