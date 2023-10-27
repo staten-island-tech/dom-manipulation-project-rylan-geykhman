30013b73b5754db6534de0ce85095b00929e51a1
rylan-geykhman <johnsmith102508@gmail.com>
2023-10-26 22:06:48 -0400

    step 1 done

--- dom.js
+++ dom.js
@@ -1,35 +1,40 @@
 const DOMSelectors = {
     form: document.querySelector('.form'),
     firstName: document.getElementById("firstName"),
+    picture: document.getElementById("picture"),
+    email: document.getElementById("email"),
     button: document.getElementById("button"),
     container: document.getElementById("container")
 };
 
 DOMSelectors.form.addEventListener("submit", function (event) {
-    event.preventDefault();
+    event.preventDefault()
     create()
 });
 
 function create() {
     const thing = {
-        name: DOMSelectors.firstName.value
+        names: DOMSelectors.firstName.value,
+        pictures: DOMSelectors.picture.value,
+        emails: DOMSelectors.email.value,
     };
-    insert(thing)
+    insert(thing, DOMSelectors);
 }
 
-function insert(thing) {
-    thing.container.insertAdjacentHTML(
-        "afterbegin",
-        `
-        <div class="card">
-            <h2 class="h2">${DOMSelectors.firstName}</h2>
-        </div>
-        `
-    );
-}
+function insert(thing, DOMSelectors) {
+    DOMSelectors.container.insertAdjacentHTML("afterbegin", 
+    ` <div class="card">
+        <h2 class="card-title">${thing.names}</h2>
+            <img src="${thing.pictures}" alt="picture" class="card-img">
+        <h3 class="card-emails">${thing.emails}</h3>
+    </div>`);
+    clear(DOMSelectors);
+};
 
+function clear(DOMSelectors){
+    DOMSelectors.name.value = DOMSelectors.picture.value = DOMSelectors.email.value = '';
+};
 
+//function pushDOM(DOMSelectors){}
 
-//const ... = make...()
-//function ...()
-//remove button
+//function removeObject(DOMSelectors){}
